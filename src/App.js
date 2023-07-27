1a9b49c02613b2741c7c911a76f6cabb4ba82725
mahan <nezafatmahan@gmail.com>
2023-07-27 16:58:26 +0330

    essential components and assets added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import ListItems from './components/list/ListItems';
+import Stat from './components/Stat';
+import './index.css';
 
 const App = () => {
   return ( 
     <>
+      <Header />
+      <Form />
+      <ListItems />
+      <Stat />
     </>
    );
 }
---
+++ src/components/Stat.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stat = () => {
+    return ( 
+        <>
+     
+        </>
+     )
+}
+ 
+export default Stat;
